3055486ab1ede24e6096119faf519f6dcd0010a4
justmolliecate <mollie.cate@gmail.com>
2018-09-06 21:06:44 -0600

    hw-3

---
+++ hw-3/sketch.js
@@ -0,0 +1,15 @@
+// setup function
+function setup() {
+
+    // created a canvas that is 250x530
+    createCanvas( 250, 530 );
+
+    // made the background teal
+    background( 'teal' );
+}
+
+function draw() {
+  //doing the draw function
+  //it will be at x=90 and y=20
+  text("Hello World", 90, 20);
+}
